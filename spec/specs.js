49bbcdb5f5f1b6af19581306e9fee47ec6942b6b
Alexa <amzeazas@comcast.net>
2015-09-08 11:53:17 -0700

    Add spec for returning equilateral if all sides are equal.

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('triangleTracker', function() {
   it("is false if it is not a triangle", function() {
     expect(triangleTracker(2, 2, 8)).to.equal(false);
   });
+
+  it("returns equilateral if all sides are equal", function() {
+    expect(triangleTracker(2, 2, 2)).to.equal(equilateral);
+  });
 });
